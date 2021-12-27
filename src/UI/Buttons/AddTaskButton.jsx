09e92aa0edef73d59f174f6cd1e2c7901108b42b
Komolov Y <komolovua@gmail.com>
2021-12-27 19:01:11 +0200

    Developed multiple versions of NewTaskButton

--- src/UI/Buttons/AddTaskButton.jsx
+++ src/UI/Buttons/AddTaskButton.jsx
@@ -1,13 +1,12 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import styled from "styled-components";
 
-export default function AddTaskButton() {
-
+export default function AddTaskButton(color) {
   return (
     <>
       <StyledButton>
         <IconWrapper>
-          <IconPlus/>
+          <IconPlus />
         </IconWrapper>
         Add Task
       </StyledButton>
@@ -17,17 +16,17 @@ export default function AddTaskButton() {
 
 const StyledButton = styled.button`
   border: none;
-  color: grey;
+  color: ${({ color }) => color || "blue"};
   cursor: pointer;
   display: flex;
   align-items: center;
   margin: 15px 0 0 0;
-  &:hover{
+  &:hover {
     color: #1e78fa;
-    & div{
+    & div {
       background-color: #1e78fa;
     }
-    & svg path{
+    & svg path {
       fill: #fff;
     }
   }
@@ -40,15 +39,20 @@ const IconWrapper = styled.div`
   margin-right: 5px;
   display: flex;
   align-items: center;
-  justify-content: center;  
-  & svg{
+  justify-content: center;
+  & svg {
     z-index: 2;
   }
 `;
 
-const IconPlus = () =>{
-  return <svg width="13" height="13">
-    <path d="M6 6V.5a.5.5 0 0 1 1 0V6h5.5a.5.5 0 1 1 0 1H7v5.5a.5.5 0 1 1-1 0V7H.5a.5.5 0 0 1 0-1H6z" fill="#1e78fa" fillRule="evenodd">
-    </path>
-  </svg>
-}
+const IconPlus = () => {
+  return (
+    <svg width="13" height="13">
+      <path
+        d="M6 6V.5a.5.5 0 0 1 1 0V6h5.5a.5.5 0 1 1 0 1H7v5.5a.5.5 0 1 1-1 0V7H.5a.5.5 0 0 1 0-1H6z"
+        fill="#1e78fa"
+        fillRule="evenodd"
+      ></path>
+    </svg>
+  );
+};
